9177a04e13d4ee155b73864a5d82f4c9745a2301
irollandDS <irolland@datasciences.ca>
2017-10-19 19:38:03 -0400

    RIT Friday Session Code
    Initial commit

---
+++ dataRIT.R
@@ -0,0 +1,12 @@
+#load packages
+#library(dplyr)
+#library(ggplot2)
+#library(plyr)
+#library(magrittr)
+#library(rvest)
+
+#load data from Downloads
+ritPP <- read.csv("/Users/Ingrid/Downloads/pp.csv")
+ritPK <- read.csv("/Users/Ingrid/Downloads/pp.csv")
+ritES <- read.csv("/Users/Ingrid/Downloads/pp.csv")
+ritALL <- read.csv("/Users/Ingrid/Downloads/pp.csv")
